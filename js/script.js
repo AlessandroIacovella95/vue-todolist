44fd6b434c4ec0063d35c9ea1209032d6e9cc5b7
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-08-31 17:03:32 +0200

    add setTimeOut interval alert show

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+        interval:false,
         newTask:'',
         title: 'TodoList',
         alert:{
@@ -45,21 +46,40 @@ createApp({
             text: this.newTask.trim(),
             done: false,
         };
-        
-        
+    
         if (toDo.text.length < 5) {
             this.alert.type ='danger',
             this.alert.message ='Errore: Inserisci almeno 5 caratteri'
             this.alert.show= true
             
+            if (!this.interval) {
+                this.interval = setTimeout(()=>{
+                    this.alert.show= false
+                },3000);  
+            }
+
             return
         }
 
+        if (this.interval) {
+            clearTimeout(this.interval);
+            this.interval= false;  
+        }
+
+        
         this.alert.type='success';
         this.alert.message='Task aggiunta con successo';
         this.alert.show=true;
         this.todoList.unshift(toDo);  
         this.newTask='';
+
+        if (!this.interval) {
+            this.interval = setTimeout(()=>{
+                this.alert.show= false
+            },3000);  
+        }
+
+        
     },
 }
 }).mount('#app')
