992127ea5fc12c8f7c31410da993aa716f2dd4bf
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-08-31 16:24:46 +0200

    add: milestone 3

--- js/script.js
+++ js/script.js
@@ -3,7 +3,14 @@ const { createApp } = Vue
 createApp({
   data() {
     return {
+        newTask:'',
         title: 'TodoList',
+        alert:{
+            message:'',
+            show: false,
+            type:','
+
+        },
         todoList : [
             {
                 text: 'Fare la spesa',
@@ -31,6 +38,28 @@ createApp({
   methods: {
     deleteTask(index){
         this.todoList.splice(index,1);
+    },
+
+    addToDo(){
+        const toDo = {
+            text: this.newTask.trim(),
+            done: false,
+        };
+        
+        
+        if (toDo.text.length < 5) {
+            this.alert.type ='danger',
+            this.alert.message ='Errore: Inserisci almeno 5 caratteri'
+            this.alert.show= true
+            
+            return
+        }
+
+        this.alert.type='success';
+        this.alert.message='Task aggiunta con successo';
+        this.alert.show=true;
+        this.todoList.unshift(toDo);  
+        this.newTask='';
     }
-  }
+}
 }).mount('#app')
